3d60319b0a29f781e4f6de12b2daf7b72ed770dd
Rorical <46294886+Rorical@users.noreply.github.com>
2023-04-03 23:48:40 +0800

    fix: delete announce not update

--- src/utils/checkAnnounce.js
+++ src/utils/checkAnnounce.js
@@ -11,7 +11,8 @@ export default async function () {
   const oldAnno = JSON.parse(storage.get(AnnounceKey, "{}"));
 
   const theNew = Lodash.difference(Object.keys(newAnno), Object.keys(oldAnno));
-  if (theNew.length > 0) {
+  const theOld = Lodash.difference(Object.keys(oldAnno), Object.keys(newAnno));
+  if (theNew.length > 0 || theOld.length > 0) {
     storage.set(AnnounceKey, JSON.stringify(newAnno));
   }
   return [theNew, newAnno];
