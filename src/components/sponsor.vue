53dcbdcb5e1fdc9ccf6e132eab3bd05b6f241365
EL_File4138 <elfile4138@outlook.com>
2023-04-22 11:39:22 +0800

    feat: Sponsor message change For an unexpected large donation

--- src/components/sponsor.vue
+++ src/components/sponsor.vue
@@ -5,13 +5,18 @@
       <button type="button" class="delete" @click="$emit('close')" />
     </header>
     <section class="modal-card-body">
-      <p>爱不会消失，只会传递</p>
+      <p>爱不会消失，只会传递。</p>
       <p>Pixivel的成长离不开大家的帮助，服务器依靠大家的赞助而活。</p>
       <p>
         赞助请点击:
         <a href="https://afdian.net/a/rorical" target="_blank"
           >https://afdian.net/a/rorical</a
         >
+      </p>
+      <p>
+        <b
+          >我们建议大于￥500的长期/一次性赞助直接与我们联系，以减少平台费率损耗同时对接更多要求。</b
+        ><i>（Tips：大于一定额度的赞助可以请求定制功能）</i>
       </p>
       <p>最近赞助会展示在下方哦！</p>
       <hr />
