17fc1b4fef34366975a1d8b515487f2be8bc679e
Rorical <46294886+Rorical@users.noreply.github.com>
2024-05-12 05:37:09 +0100

    Revert "change ads & analytics"
    This reverts commit 2c9447f5522a840010686bbca96c84c22325b196.

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -14,9 +14,14 @@
           >Privacy</a
         >
       </p>
-
-      <p ref="adbanner">这里应该会有个广告用来支持运营</p>
-      <div id="adcontainer"></div>
+      <!--<p ref="adbanner">这里应该会有个广告用来支持运营</p>
+      <ins
+        class="adsbygoogle"
+        style="display: block"
+        data-ad-client="ca-pub-7564284646329791"
+        data-ad-slot="6050058983"
+        data-ad-format="auto"
+      ></ins>-->
     </div>
   </footer>
 </template>
@@ -27,10 +32,9 @@ import Banned from "@/assets/info/ban.json";
 export default {
   name: "Footer",
   mounted() {
-    this.initAna();
-    if (Banned.indexOf(window.location.href) === -1) {
+    if (Banned.indexOf(window.location.href) == -1) {
       const cancel = setTimeout(() => {
-        this.initAds();
+        //this.initAds();
       }, 3000);
       const cancelAdsLoadHook = () => {
         clearTimeout(cancel);
@@ -41,33 +45,39 @@ export default {
   },
   methods: {
     initAds() {
-      window.atOptions = {
-        key: "5bd04ec1ba9e7d164ae3212efc50d3e8",
-        format: "iframe",
-        height: 50,
-        width: 320,
-        params: {},
-      };
       let adscript = document.createElement("script");
       adscript.type = "text/javascript";
+      adscript.onload = () => {
+        try {
+          (window.adsbygoogle || []).push({});
+        } catch (error) {
+          this.$refs.adbanner.innerText =
+            "请考虑对本站点解除您的AdBlock以支持本站点长期发展。";
+          console.error(error);
+        }
+      };
       adscript.onerror = () => {
         this.$refs.adbanner.innerText =
           "我们理解您对互联网广告的厌恶，我们也在不断探索更加用户友好的运营策略；但目前，我们依然依赖于广告收入支撑服务运行。请考虑对本站点解除您的AdBlock，或者选择捐赠以支持本站点长期发展。";
       };
+      adscript["data-ad-client"] = "ca-pub-7564284646329791";
       adscript.src =
-        "//www.topcreativeformat.com/5bd04ec1ba9e7d164ae3212efc50d3e8/invoke.js";
-      window.document.getElementById("adcontainer").appendChild(adscript);
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7564284646329791";
+      document.head.appendChild(adscript);
     },
     initAna() {
       let analyticscript = document.createElement("script");
       analyticscript.type = "text/javascript";
-      analyticscript.src = "https://analytics.rorical.blue/script.js";
-      analyticscript.async = true;
-      analyticscript.defer = true;
-      analyticscript.setAttribute(
-        "data-website-id",
-        "3be74123-a4c6-493e-a059-266c718ac386"
-      );
+      analyticscript.onload = () => {
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          window.dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+        gtag("config", "UA-159010258-1");
+      };
+      analyticscript.src =
+        "https://www.googletagmanager.com/gtag/js?id=UA-159010258-1";
       document.head.appendChild(analyticscript);
     },
   },
